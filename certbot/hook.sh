01b930298623a8464c8458adc5c70eea17c1480f
Lechros <0onionion0@gmail.com>
2025-01-08 04:00:26 +0900

    fix: fix oci lb certificate command

--- certbot/hook.sh
+++ certbot/hook.sh
@@ -1,5 +1,8 @@
-oci lb certificate update \
+current_date=$(date + "%Y-%m-%d")
+name="${CERTIFICATE_NAME}_${current_date}"
+
+oci lb certificate create \
   --load-balancer-id ${LOAD_BALANCER_OCID} \
-  --certificate-name ${CERTIFICATE_NAME} \
+  --certificate-name ${name} \
   --certificate-file ${RENEWED_LINEAGE}/fullchain.pem \
   --private-key-file ${RENEWED_LINEAGE}/privkey.pem
