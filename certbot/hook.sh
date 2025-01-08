a9f2a50d9a79334bf024e50871223accd02db017
Lechros <0onionion0@gmail.com>
2025-01-08 15:34:10 +0900

    fix: move oci func to seperate container

--- certbot/hook.sh
+++ certbot/hook.sh
@@ -1,8 +1,9 @@
 current_date=$(date + "%Y-%m-%d")
 name="${CERTIFICATE_NAME}_${current_date}"
 
-oci lb certificate create \
-  --load-balancer-id ${LOAD_BALANCER_OCID} \
-  --certificate-name ${name} \
-  --certificate-file ${RENEWED_LINEAGE}/fullchain.pem \
-  --private-key-file ${RENEWED_LINEAGE}/privkey.pem
+sshpass -p root ssh -o StrictHostKeyChecking=no root@${OCI_CONTAINER_NAME} \
+  oci lb certificate create \
+    --load-balancer-id ${LOAD_BALANCER_OCID} \
+    --certificate-name ${name} \
+    --certificate-file ${RENEWED_LINEAGE}/fullchain.pem \
+    --private-key-file ${RENEWED_LINEAGE}/privkey.pem
