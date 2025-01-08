e82a1380f4cecb2693f6bd517bc737170aa52d58
Lechros <0onionion0@gmail.com>
2025-01-08 15:57:45 +0900

    fix: send key file to container

--- certbot/hook.sh
+++ certbot/hook.sh
@@ -1,9 +1,20 @@
 current_date=$(date +"%Y-%m-%d")
 name="${CERTIFICATE_NAME}_${current_date}"
 
+sshpass -p root scp -o StrictHostKeyChecking=no \
+  ${RENEWED_LINEAGE}/fullchain.pem \
+  root@${OCI_CONTAINER_NAME}:/tmp/fullchain.pem
+
+sshpass -p root scp -o StrictHostKeyChecking=no \
+  ${RENEWED_LINEAGE}/privkey.pem \
+  root@${OCI_CONTAINER_NAME}:/tmp/privkey.pem
+
 sshpass -p root ssh -o StrictHostKeyChecking=no root@${OCI_CONTAINER_NAME} \
   oci lb certificate create \
     --load-balancer-id ${LOAD_BALANCER_OCID} \
     --certificate-name ${name} \
-    --public-certificate-file ${RENEWED_LINEAGE}/fullchain.pem \
-    --private-key-file ${RENEWED_LINEAGE}/privkey.pem
+    --public-certificate-file /tmp/fullchain.pem \
+    --private-key-file /tmp/privkey.pem
+
+sshpass -p root ssh -o StrictHostKeyChecking=no root@${OCI_CONTAINER_NAME} \
+  rm -f /tmp/fullchain.pem /tmp/privkey.pem
