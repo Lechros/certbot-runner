774edc33387dde8afc2b4ad629f2a08dfb74545d
Lechros <0onionion0@gmail.com>
2025-01-08 17:02:01 +0900

    fix: move envs to certbot

--- certbot/hook.sh
+++ certbot/hook.sh
@@ -1,5 +1,3 @@
-
-
 sshpass -p root scp -o StrictHostKeyChecking=no \
   ${RENEWED_LINEAGE}/fullchain.pem \
   root@${OCI_CONTAINER_NAME}:/tmp/fullchain.pem
@@ -8,7 +6,14 @@ sshpass -p root scp -o StrictHostKeyChecking=no \
   ${RENEWED_LINEAGE}/privkey.pem \
   root@${OCI_CONTAINER_NAME}:/tmp/privkey.pem
 
+current_date=$(date +"%Y.%m.%d")
+name="${CERTIFICATE_NAME}_$current_date"
+
 sshpass -p root ssh -o StrictHostKeyChecking=no root@${OCI_CONTAINER_NAME} << EOF
-  ./create-certificate.sh
+  oci lb certificate create \
+    --load-balancer-id ${LOAD_BALANCER_OCID} \
+    --certificate-name $name \
+    --public-certificate-file /tmp/fullchain.pem \
+    --private-key-file /tmp/privkey.pem
   rm -f /tmp/fullchain.pem /tmp/privkey.pem
 EOF
--- oci/create-certificate.sh
+++
@@ -1,8 +0,0 @@
-current_date=$(date +"%Y.%m.%d")
-name="${CERTIFICATE_NAME}_$current_date"
-
-oci lb certificate create \
-  --load-balancer-id ${LOAD_BALANCER_OCID} \
-  --certificate-name $name \
-  --public-certificate-file /tmp/fullchain.pem \
-  --private-key-file /tmp/privkey.pem
