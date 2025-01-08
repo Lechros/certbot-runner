f5420f9ac8301377d9a3446b9b6056e3551f4449
Lechros <0onionion0@gmail.com>
2025-01-08 16:11:40 +0900

    fix: really escape name

--- certbot/hook.sh
+++ certbot/hook.sh
@@ -1,6 +1,8 @@
 current_date=$(date +"%Y-%m-%d")
 name="${CERTIFICATE_NAME}-${current_date}"
 
+escaped_name=$(printf "%q" "${name}")
+
 sshpass -p root scp -o StrictHostKeyChecking=no \
   ${RENEWED_LINEAGE}/fullchain.pem \
   root@${OCI_CONTAINER_NAME}:/tmp/fullchain.pem
@@ -12,7 +14,7 @@ sshpass -p root scp -o StrictHostKeyChecking=no \
 sshpass -p root ssh -o StrictHostKeyChecking=no root@${OCI_CONTAINER_NAME} \
   oci lb certificate create \
     --load-balancer-id ${LOAD_BALANCER_OCID} \
-    --certificate-name "${name}" \
+    --certificate-name "${escaped_name}" \
     --public-certificate-file /tmp/fullchain.pem \
     --private-key-file /tmp/privkey.pem
 
