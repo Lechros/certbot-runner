79bbd93a9a4c55a66e62835eb50272cc6050fb03
Lechros <0onionion0@gmail.com>
2025-01-08 16:40:07 +0900

    feat: just remove dashes from name

--- certbot/hook.sh
+++ certbot/hook.sh
@@ -1,5 +1,5 @@
-current_date=$(date +"%Y-%m-%d")
-name="${CERTIFICATE_NAME}-$current_date"
+current_date=$(date +"%Y.%m.%d")
+name="${CERTIFICATE_NAME}_$current_date"
 
 sshpass -p root scp -o StrictHostKeyChecking=no \
   ${RENEWED_LINEAGE}/fullchain.pem \
@@ -12,7 +12,7 @@ sshpass -p root scp -o StrictHostKeyChecking=no \
 sshpass -p root ssh -o StrictHostKeyChecking=no root@${OCI_CONTAINER_NAME} << EOF
   oci lb certificate create \
     --load-balancer-id ${LOAD_BALANCER_OCID} \
-    --certificate-name "$name" \
+    --certificate-name $name \
     --public-certificate-file /tmp/fullchain.pem \
     --private-key-file /tmp/privkey.pem
   rm -f /tmp/fullchain.pem /tmp/privkey.pem
